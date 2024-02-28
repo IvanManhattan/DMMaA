c4a72231f88f2b6d0b3712bc654cd58c3aa49af4
IvanManhattan <IvanManhattan>
2024-02-28 20:06:17 +0300

    Interface done

--- Labs/src/lab1/GUIClass.java
+++ Labs/src/lab1/GUIClass.java
@@ -14,26 +14,16 @@ public class GUIClass extends JFrame {
     private List<ObjectClass> centres;
     private int classesCount = 7;
     private int elementsCount = 14000;
+    private boolean isRepaint = false;
 
     public GUIClass() {
         super("Lab 1");
-        setBounds(100, 100, 800, 600);
+        setBounds(100, 100, 500, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         initElements();
 
-        actionButtonCreateAverage = new JButton("Average");
-        actionButtonCreateAverage.setBounds(50,100,95,30);
-
-        actionButtonCreateMaximin = new JButton("Maximin");
-        actionButtonCreateMaximin.setBounds(50,100,95,30);
-
-        actionButtonCreateAverage.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
+        JPanel actionPanel = new JPanel();
 
         JPanel panel = new JPanel() {
 
@@ -44,8 +34,16 @@ public class GUIClass extends JFrame {
                 int pointWidth = 3;
                 int pointHeight = 3;
 
-                for (int i = 0; i < elementsCount; i++) {
-                    if (!elements.get(i).isCenter()) {
+                if (!isRepaint) {
+                    for (int i = 0; i < elementsCount; i++) {
+                        if (!elements.get(i).isCenter()) {
+                            pointWidth = 3;
+                            pointHeight = 3;
+                        }
+                        else {
+                            pointWidth = 10;
+                            pointHeight = 10;
+                        }
                         if (elements.get(i).getColor() == 0)
                             g.setColor(Color.WHITE);
                         else if (elements.get(i).getColor() == 1)
@@ -62,33 +60,32 @@ public class GUIClass extends JFrame {
                             g.setColor(Color.MAGENTA);
                         else if (elements.get(i).getColor() == 7)
                             g.setColor(Color.ORANGE);
-                        pointWidth = 3;
-                        pointHeight = 3;
+
+                        g.fillOval(elements.get(i).getX(), elements.get(i).getY(), pointWidth, pointHeight);
                     }
-                    else {
-                        if (elements.get(i).getColor() == 1)
-                            g.setColor(Color.RED);
-                        else if (elements.get(i).getColor() == 2)
-                            g.setColor(Color.BLUE);
-                        else if (elements.get(i).getColor() == 3)
-                            g.setColor(Color.YELLOW);
-                        else if (elements.get(i).getColor() == 4)
-                            g.setColor(Color.GREEN);
-                        else if (elements.get(i).getColor() == 5)
-                            g.setColor(Color.PINK);
-                        else if (elements.get(i).getColor() == 6)
-                            g.setColor(Color.MAGENTA);
-                        else if (elements.get(i).getColor() == 7)
-                            g.setColor(Color.ORANGE);
-                        pointWidth = 10;
-                        pointHeight = 10;
-                    }
-                    g.fillOval(elements.get(i).getX(), elements.get(i).getY(), pointWidth, pointHeight);
                 }
+                else {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(0, 0, 500, 500);
+                }
+
             }
         };
 
-        JPanel actionPanel = new JPanel();
+        actionButtonCreateAverage = new JButton("Average");
+        actionButtonCreateAverage.setBounds(50,100,95,30);
+
+        actionButtonCreateMaximin = new JButton("Maximin");
+        actionButtonCreateMaximin.setBounds(50,100,95,30);
+
+        actionButtonCreateAverage.addActionListener( e-> {
+            isRepaint = true;
+            panel.repaint();
+            averageKMethod();
+            isRepaint = false;
+            panel.repaint();
+        });
+
 
         this.add(panel, BorderLayout.CENTER);
         this.add(actionPanel, BorderLayout.SOUTH);
@@ -100,6 +97,8 @@ public class GUIClass extends JFrame {
     }
 
     private void initElements() {
+
+        centres = new ArrayList<>();
         elements = new ArrayList<>();
         Random rnd = new Random();
 
@@ -108,48 +107,45 @@ public class GUIClass extends JFrame {
                     rnd.nextInt(0, 500), rnd.nextInt(0, 500), 0, false));
         }
 
-        List<ObjectClass> previous = new ArrayList<>();
+        // List<ObjectClass> previous = new ArrayList<>();
 
+        // Инициализируем центры - каждый центр имеет свой цвет, затем
+        // элементам будут присваиваться цвета в зависимости от центра
         for (int j = 0; j < classesCount; j++) {
             ObjectClass tempElement = elements.get(rnd.nextInt(0, 14000));
-            if (!previous.contains(tempElement)) {
+            if (!centres.contains(tempElement)) {
                 tempElement.setCenter(true);
                 tempElement.setColor(j + 1);
-                previous.add(tempElement);
+                centres.add(tempElement);
             }
         }
 
-        centres = (ArrayList<ObjectClass>) ((ArrayList<ObjectClass>) previous).clone();
+        // centres = (ArrayList<ObjectClass>) ((ArrayList<ObjectClass>) previous).clone();
 
     }
 
     private void averageKMethod() {
 
         float minDistance = 10000.0F;
-        int cluster = 0;
+        int cluster;
 
         for (int i = 0; i < elementsCount; i++) {
+            cluster = 0;
+            minDistance = 10000.0F;
             for (int j = 0; j < classesCount; j++) {
                 float distance = (float) Math.sqrt((Math.pow(elements.get(i).getX() - centres.get(j).getX(), 2)) +
                         Math.pow(elements.get(i).getY() - centres.get(j).getY(), 2));
                 if (minDistance > distance) {
                     minDistance = distance;
-                    cluster = j + 1;
+                    cluster = j;
                 }
             }
-            elements.get(i).setColor(cluster);
+            elements.get(i).setColor(cluster + 1);
         }
     }
 
     private void maximinMethod() {
 
-    }
-
-    private void repaint(Graphics g) {
-        g.fillRect(0, 0, 500, 500);
-        for (int i = 0; i < elementsCount; i++) {
-            // TODO
-        }
     }
 
 }
